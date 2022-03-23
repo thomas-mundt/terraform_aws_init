67e224fb02a8db69a3218d01c61e5d48fb6ceeb9
Thomas Mundt <mundt_thomas@yahoo.de>
2022-03-23 12:03:04 +0100

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -3,11 +3,42 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "~> 3.28"
+      version = "~> 3.66"
     }
-    random = {
-      source = "hashicorp/random"
-      version = "~> 3.0"
-    }
+    
+    //random = {
+    //  source = "hashicorp/random"
+    //  version = "~> 3.0"
+    //}
+   
+    //local = {
+    //  source = "hashicorp/local"
+    //  version = "~> 2.0"
+    //}
+    
+    //cloudinit = {
+    //  source  = "hashicorp/cloudinit"
+    //  version = "~> 2.1"
+    //}
+
+    //archive = {
+    //  source  = "hashicorp/archive"
+    //  version = "~> 2.0"
+    //}
+    
+    //kubernetes = {
+    //  source  = "hashicorp/kubernetes"
+    //  version = ">= 2.6.1"
+    //}
+    
+    //helm = {
+    //  source  = "hashicorp/helm"
+    //  version = ">= 2.4.1"
+    //}
   }
+  
+  backend "local" {
+    path = "local_tf_state/terraform.tfstate"
+  }
+  
 }
